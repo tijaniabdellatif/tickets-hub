7b395b21057eac72abd9246562a73cae7ecf3326
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2024-01-24 14:09:28 +0100

    deployment configuration

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,10 +2,13 @@ import express,{Request,Response,NextFunction} from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 require('dotenv').config();
-import morgan from 'morgan';
-
 
 export const app = express();
 app.use(express.json({limit:'50mb'}));
 app.use(cookieParser());
 app.use(cors());
+
+
+app.get('api/users/currentuser',(req:Request,res:Response)=> {
+    res.send("hi there !!");
+});
