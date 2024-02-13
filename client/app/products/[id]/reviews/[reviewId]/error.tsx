09af7f52c30830fca05d1f4edeeb795dcfe69bb7
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2024-02-14 00:33:03 +0100

    the reset function error

---
+++ client/app/products/[id]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+type ErrorStack = {
+
+  error:Error,
+  reset: () => void
+}
+export default function ErrorBoundary({error,reset}:ErrorStack){
+
+      return (
+        <>
+         <h1>{error.message}</h1> 
+         <button onClick={reset}>Try again</button> 
+   
+        </>
+      )
+}
--- client/app/products/[id]/reviews/[reviewId]/page.tsx
+++ client/app/products/[id]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,20 @@
+"use client"
 import {notFound} from 'next/navigation'
 
+
+function getRandomInt(count:number):number{
+
+  return Math.floor(Math.random() * count);
+}
 export default function ReviewDetails({params}:{params:{id:string,reviewId:string}}){
 
+
+   const random = getRandomInt(2);
+
+   if(random === 1){
+
+       throw new Error('Error loading review');
+   }
  
    if(parseInt(params.reviewId) > 1000){
 
