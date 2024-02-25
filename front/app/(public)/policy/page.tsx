5094988885981d3eefce616701a47175befd56f2
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2024-02-25 22:35:09 +0100

    using twJoin() to join classes into subclasses with conditional styles

--- front/app/(public)/policy/page.tsx
+++ front/app/(public)/policy/page.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import {twJoin} from "tailwind-merge"
 type Props = {}
 
 const PolicyPage = (props: Props) => {
+
+  const isClass = false;
   return (
-    <div>page</div>
+    <div className={twJoin(
+        "text-black",
+        isClass && "bg-red-500 h-[80px]"
+      
+    )}>
+
+      <h2>Hello world</h2>
+    </div>
   )
 }
 
